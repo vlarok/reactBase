e597dbf9e2d3112bbc2d60871d14968caeb8b6f9
Vladimir Rõkovanov <vladimir.rokovanov@helmes.ee>
2017-01-02 12:33:07 +0200

    Hello React!

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+var Hello = React.createClass({
+  render: function (){
+    return React.createElement("h3",null,"Hello React!");
+  }
+});
+
+ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
